8e3fbdc9caf19e0c6c4737060fd43ff14d31b15d
demo <nasima365banu@gmail.com>
2026-07-01 23:49:23 +0530

    Completed Spring Core Exercises 1

---
+++ src/main/java/com/example/SpringMavenExercise/aspect/LoggingAspect.java
@@ -0,0 +1,4 @@
+package com.example.SpringMavenExercise.aspect;
+
+public class LoggingAspect {
+}
---
+++ src/test/java/com/example/SpringMavenExercise/SpringMavenExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringMavenExercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMavenExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
