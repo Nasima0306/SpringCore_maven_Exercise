1f6786f73cd906ee2e6e0f014c273667c359b0ba
demo <nasima365banu@gmail.com>
2026-07-02 10:23:44 +0530

    Exercise 4 Maven Project Configuration

---
+++ src/main/java/com/example/SpringMavenExercise/Exercise4/MavenConfigurationDemo.java
@@ -0,0 +1,7 @@
+package com.example.SpringMavenExercise.Exercise4;
+
+public class MavenConfigurationDemo {
+    public static void main(String[] args) {
+        System.out.println("Maven Project Configured Successfully");
+    }
+}
